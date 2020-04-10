71a4dbf10bc39e6da9f9bc55ceacdd124dc2b6de
Otto <otto@ot-to.nl>
2020-04-10 14:40:23 +0200

    `ping`: ipv6 support, resolve addresses, force ipv4/6 flag
    ipv6 does not properly set sequence numbers yet

--- src/bin/ping.rs
+++ src/bin/ping.rs
@@ -3,14 +3,15 @@
 //! Warning, needs to be run with `sudo`
 //!
 //! Todo:
-//!  - resolve addrs
-//!  - support IPv6
+//!  - fix ipv6 sequence number (payload)
+//!  - use icmp identifier so you can run `ping` bins concurrently
 //!  - report ttl, damaged replies
 //!  - all other options
 
 use coreutils::{emit_bell, print_help_and_exit};
 
 use std::net::IpAddr;
+use std::net::ToSocketAddrs;
 use std::process::exit;
 use std::time::Duration;
 use std::time::Instant;
@@ -22,14 +23,16 @@ use std::thread;
 use pnet::packet::icmp::echo_reply::EchoReplyPacket;
 use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
 use pnet::packet::icmp::IcmpTypes;
+use pnet::packet::icmpv6::Icmpv6Types;
+use pnet::packet::icmpv6::{Icmpv6Packet, MutableIcmpv6Packet};
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::util;
 use pnet::packet::Packet;
 use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::{Ipv4, Ipv6};
-use pnet::transport::{icmp_packet_iter, icmpv6_packet_iter, transport_channel};
+use pnet::transport::{icmp_packet_iter, icmpv6_packet_iter, transport_channel, TransportReceiver};
 
-const USAGE: &str = "ping <dest>: send ICMP ECHO_REQUEST to network hosts";
+const USAGE: &str = "ping [-a] [-4|-6] <dest>: send ICMP ECHO_REQUEST to network hosts";
 
 /// State of the ICMP echo request
 enum Echo {
@@ -39,29 +42,44 @@ enum Echo {
     Received(u128),
 }
 
+/// IP version number
+enum IpVersion {
+    V4,
+    V6,
+}
+
 /// Parse arguments, run job, pass return code
 fn main() -> ! {
     let mut args = std::env::args();
     args.next(); // bin name
 
     let mut audible = false;
-    let dest = match args.next() {
-        Some(s) if &s == "-a" => match args.next() {
-            Some(dest) => {
-                audible = true;
-                dest
-            }
-            None => print_help_and_exit(USAGE),
-        },
-        Some(dest) => dest,
-        None => print_help_and_exit(USAGE),
-    };
+    let mut pref_ip_version = None;
+    let mut dest = None;
+
+    while let Some(arg) = args.next() {
+        if &arg == "-a" {
+            audible = true;
+        } else if &arg == "-4" {
+            pref_ip_version = Some(IpVersion::V4);
+        } else if &arg == "-6" {
+            pref_ip_version = Some(IpVersion::V6);
+        } else {
+            dest = Some(arg);
+            break;
+        }
+    }
 
     if args.next().is_some() {
         print_help_and_exit(USAGE);
     }
 
-    match ping(dest, audible) {
+    let dest = match dest {
+        None => print_help_and_exit(USAGE),
+        Some(dest) => dest,
+    };
+
+    match ping(dest, audible, pref_ip_version) {
         Ok(0) => exit(2), // no replies received
         Ok(_) => exit(0), // replies received
         Err(e) => {
@@ -71,54 +89,148 @@ fn main() -> ! {
     }
 }
 
-/// `ping` implementation for ipv4
-fn ping(dest: String, audible: bool) -> Result<usize, Box<dyn std::error::Error>> {
-    let ip: IpAddr = dest.parse()?;
-    let pings = Arc::new(Mutex::new(vec![]));
+/// register received echo reply
+fn register_echo_reply(slot: Option<&mut Echo>) -> Option<u128> {
+    match slot {
+        None => {
+            eprintln!("received unsollicited ICMP echo response");
+            None
+        }
+        Some(Echo::Received(_)) => {
+            eprintln!("received duplicate response");
+            None
+        }
+        Some(echo) => {
+            let sent = match *echo {
+                Echo::Sent(sent) => sent,
+                _ => unreachable!(),
+            };
+            let ping = Instant::now().duration_since(sent).as_millis();
+            *echo = Echo::Received(ping);
 
-    let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
-    let (mut tx, mut rx) = transport_channel(4096, protocol)?;
+            Some(ping)
+        }
+    }
+}
+
+/// IPv4 ICMP reply handling
+fn listen_ipv4(mut rx: TransportReceiver, audible: bool, pings_recv: Arc<Mutex<Vec<Echo>>>) -> ! {
+    let mut iter = icmp_packet_iter(&mut rx);
+    while let Ok((packet, addr)) = iter.next() {
+        if audible {
+            emit_bell();
+        }
+
+        let echo_reply = EchoReplyPacket::new(packet.packet()).unwrap();
+        let sequence_number = echo_reply.get_sequence_number();
+
+        // todo better handling of sequence_number 0
+        let mut lock = pings_recv.lock().unwrap();
+        let slot = lock.get_mut(sequence_number as usize - 1);
+        if let Some(ping) = register_echo_reply(slot) {
+            println!(
+                "{} bytes from {}: icmp_seq={} ttl=.. time={} ms",
+                packet.packet().len(),
+                addr,
+                sequence_number,
+                ping
+            );
+        }
+    }
+
+    panic!("thread listener quit unexpectedly");
+}
+
+/// IPv6 ICMP reply handling
+fn listen_ipv6(mut rx: TransportReceiver, audible: bool, pings_recv: Arc<Mutex<Vec<Echo>>>) -> ! {
+    let mut iter = icmpv6_packet_iter(&mut rx);
+    while let Ok((packet, addr)) = iter.next() {
+        if audible {
+            emit_bell();
+        }
+
+        let echo_reply = Icmpv6Packet::new(packet.packet()).unwrap();
+        let sequence_number = 1; // TODO
+
+        let mut lock = pings_recv.lock().unwrap();
+        let slot = lock.get_mut(sequence_number as usize - 1);
+        if let Some(ping) = register_echo_reply(slot) {
+            println!(
+                "{} bytes from {}: icmp_seq={} ttl=.. time={} ms",
+                packet.packet().len(),
+                addr,
+                sequence_number,
+                ping
+            );
+        }
+    }
+
+    panic!("thread listener quit unexpectedly");
+}
+
+fn build_ipv4_request<'a>(vec: &'a mut Vec<u8>, sequence_number: u16) -> impl Packet + 'a {
+    let mut echo_packet = MutableEchoRequestPacket::new(vec).unwrap();
+
+    echo_packet.set_identifier(0);
+    echo_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    echo_packet.set_sequence_number(sequence_number);
+
+    let checksum = util::checksum(echo_packet.packet(), 1);
+    echo_packet.set_checksum(checksum);
+
+    echo_packet
+}
+
+fn build_ipv6_request<'a>(vec: &'a mut Vec<u8>, sequence_number: u16) -> impl Packet + 'a {
+    let mut echo_packet = MutableIcmpv6Packet::new(&mut vec[..]).unwrap();
+
+    echo_packet.set_icmpv6_type(Icmpv6Types::EchoRequest);
+    // todo set payload (sequence number)
+
+    let checksum = util::checksum(echo_packet.packet(), 1);
+    echo_packet.set_checksum(checksum);
+
+    echo_packet
+}
+
+/// `ping` implementation
+fn ping(
+    dest: String,
+    audible: bool,
+    pref_ip_version: Option<IpVersion>,
+) -> Result<usize, Box<dyn std::error::Error>> {
+    let addrs_iter = format!("{}:12345", dest).to_socket_addrs()?;
+    let ip = addrs_iter
+        .map(|addr| addr.ip())
+        .find(|ip| match pref_ip_version {
+            None => true,
+            Some(IpVersion::V4) => match ip {
+                IpAddr::V4(_) => true,
+                _ => false,
+            },
+            Some(IpVersion::V6) => match ip {
+                IpAddr::V6(_) => true,
+                _ => false,
+            },
+        })
+        .expect("unable to resolve address");
+
+    let protocol = match ip {
+        IpAddr::V4(_) => Layer4(Ipv4(IpNextHeaderProtocols::Icmp)),
+        IpAddr::V6(_) => Layer4(Ipv6(IpNextHeaderProtocols::Icmpv6)),
+    };
+    let (mut tx, rx) = transport_channel(4096, protocol)?;
+
+    let pings = Arc::new(Mutex::new(vec![]));
 
     // spin up the thread that receives the ICMP echo replies
     let pings_recv = pings.clone();
-    thread::spawn(move || {
-        let mut iter = icmp_packet_iter(&mut rx);
-        while let Ok((packet, addr)) = iter.next() {
-            if audible {
-                emit_bell();
-            }
-
-            let echo_reply = EchoReplyPacket::new(packet.packet()).unwrap();
-            let sequence_number = echo_reply.get_sequence_number();
-            match pings_recv
-                .lock()
-                .unwrap()
-                .get_mut(sequence_number as usize - 1)
-            {
-                None => eprintln!("received unsollicited ICMP echo response"),
-                Some(Echo::Received(_)) => {
-                    eprintln!("received duplicate response");
-                }
-                Some(echo) => {
-                    let sent = match *echo {
-                        Echo::Sent(sent) => sent,
-                        _ => unreachable!(),
-                    };
-                    let ping = Instant::now().duration_since(sent).as_millis();
-                    println!(
-                        "{} bytes from {}: icmp_seq={} ttl=.. time={} ms",
-                        packet.packet().len(),
-                        addr,
-                        sequence_number,
-                        ping
-                    );
-                    *echo = Echo::Received(ping);
-                }
-            }
-        }
-
-        eprintln!("thread listener quit unexpectedly");
+    thread::spawn(move || match ip {
+        IpAddr::V4(_) => listen_ipv4(rx, audible, pings_recv),
+        IpAddr::V6(_) => listen_ipv6(rx, audible, pings_recv),
     });
+
+    println!("PING {} ({}): {} data bytes", dest, ip, 16);
 
     // setup CTRL-C handler at this point, not earlier
     let running = Arc::new(AtomicBool::new(true));
@@ -127,23 +239,23 @@ fn ping(dest: String, audible: bool) -> Result<usize, Box<dyn std::error::Error>
         r.store(false, Ordering::SeqCst);
     })?;
 
+    let mut vec: Vec<u8> = vec![0; 16];
     while running.load(Ordering::SeqCst) {
-        let mut vec: Vec<u8> = vec![0; 16];
-        let mut echo_packet = MutableEchoRequestPacket::new(&mut vec[..]).unwrap();
-        echo_packet.set_identifier(0);
-        echo_packet.set_icmp_type(IcmpTypes::EchoRequest);
-
         let sequence_number = {
             let mut pings = pings.lock().unwrap();
             pings.push(Echo::Sent(Instant::now()));
             pings.len() as u16 // start at 1
         };
-        echo_packet.set_sequence_number(sequence_number);
-
-        let checksum = util::checksum(echo_packet.packet(), 1);
-        echo_packet.set_checksum(checksum);
-
-        tx.send_to(echo_packet, ip)?;
+        match ip {
+            IpAddr::V4(_) => {
+                let echo_packet = build_ipv4_request(&mut vec, sequence_number);
+                tx.send_to(echo_packet, ip)?
+            }
+            IpAddr::V6(_) => {
+                let echo_packet = build_ipv6_request(&mut vec, sequence_number);
+                tx.send_to(echo_packet, ip)?
+            }
+        };
 
         // sleep for small periods and check for ctrl-c
         for _ in 0..10 {
