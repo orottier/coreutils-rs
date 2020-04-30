335ce06d0680d6c01ac93c7a4a29b16f992a25ec
Otto Rottier <ottorottier@gmail.com>
2020-04-30 12:03:31 +0200

    Started with `du`: estimate file space usage

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Linux (core) utilities rewritten in Rust, for fun and profit
 |---------|--------------------------------------------------------|------------------------------------------------------------------------------------------|------------------------------|
 | cat     | output file<br>output stdin<br>line numbers            | some print options                                                                       |                              |
 | cut     | cut bytes, chars, fields                               | multiple ranges<br>...                                                                   | clap                         |
+| du      | count and summarize paths                              | do not visit paths twice<br>symlinks<br>all other options<br>support all file systems    |                              |
 | less    | show file<br>cursor navigation<br>search and highlight | show stdin<br>searching backwards<br>terminal resizing<br>page up/down<br>tailing<br>... | termion<br>regex<br>memmap   |
 | ping    | ipv4<br>ipv6 (somewhat)<br>resolving                   | ipv6 sequence numbers<br>icmp identifiers<br>report ttl, damaged<br>...                  | pnet                         |
 | pv      | stats<br>progress bar<br>                              | ...                                                                                      | indicatif                    |
---
+++ src/bin/du.rs
@@ -0,0 +1,132 @@
+//! `du`: estimate file space usage
+//!
+//! Special effort has been put in a non-recursive program flow,
+//! instead we use a linked list of actions with a 'total size' carry.
+//!
+//! Todo:
+//! - do not visit paths twice
+//! - apparent or actual block size?
+//! - implement all options
+
+use std::os::macos::fs::MetadataExt;
+use std::path::PathBuf;
+use std::process::exit;
+
+use coreutils::util::print_help_and_exit;
+
+const USAGE: &str = "du [OPTION]... [FILE]...: estimate file space usage";
+
+/// Parse arguments, run job, pass return code
+fn main() -> ! {
+    let mut args = std::env::args();
+    args.next(); // bin name
+
+    let mut inputs: Vec<String> = args
+        .map(|arg| {
+            if arg.starts_with('-') {
+                print_help_and_exit(USAGE)
+            } else {
+                arg
+            }
+        })
+        .collect();
+
+    if inputs.is_empty() {
+        inputs.push(String::from("."));
+    }
+
+    du(inputs);
+
+    exit(0)
+}
+
+/// Task instruction to enter or summarize given path
+///
+/// This program implements `du` in a non-recursive way,
+/// instead a linked list of `DuTask`s is used.
+struct DuTask {
+    /// task to run up next
+    next: Option<Box<DuTask>>,
+    /// enter or summarize this path
+    path: PathBuf,
+    /// when true, enter this path and count files, otherwise print summary
+    enter: bool,
+}
+
+impl DuTask {
+    pub fn new(path: String) -> Self {
+        Self {
+            next: None,
+            path: PathBuf::from(path),
+            enter: true,
+        }
+    }
+
+    pub fn run(self, carry: &mut Vec<u64>) -> Option<Box<Self>> {
+        let DuTask { next, path, enter } = self;
+
+        if enter {
+            let entries_result = path.read_dir();
+
+            let summary_task = Self {
+                next,
+                path,
+                enter: false,
+            };
+
+            let mut next_holder = Some(Box::new(summary_task));
+            let mut size = 0;
+
+            if let Ok(entries) = entries_result {
+                for entry_result in entries {
+                    if let Ok(entry) = entry_result {
+                        let attr = entry.metadata().unwrap();
+
+                        if attr.is_dir() {
+                            let task = Self {
+                                next: next_holder.take(),
+                                path: entry.path(),
+                                enter: true,
+                            };
+                            next_holder = Some(Box::new(task));
+                        } else {
+                            size += attr.st_blocks();
+                        }
+                    } else {
+                        eprintln!("{:?}", entry_result.unwrap_err());
+                    }
+                }
+            } else {
+                eprintln!("{:?}", entries_result.unwrap_err());
+            }
+
+            carry.push(size);
+
+            next_holder.take()
+        } else {
+            let total_size = carry.pop().unwrap();
+            println!("{:<10} {}", total_size, path.to_string_lossy());
+
+            let prev_index = carry.len() - 1;
+            carry[prev_index] += total_size;
+
+            next
+        }
+    }
+}
+
+/// `du` implementation
+fn du(mut inputs: Vec<String>) {
+    // iterate all input paths
+    while let Some(input) = inputs.pop() {
+        let mut task_holder = Some(Box::new(DuTask::new(input)));
+
+        // This is the stack of directory sizes, the index corresponds with the depth.
+        // Start with the root dir, of size zero.
+        let mut carry = vec![0];
+
+        while let Some(task) = task_holder.take() {
+            task_holder = task.run(&mut carry);
+        }
+    }
+}
